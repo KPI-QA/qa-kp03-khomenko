d2dc55471f14496ec7abece1d6374e8644c82b2c
KhomenkoMac <maksim_khomenko@ukr.net>
2022-10-30 11:06:33 +0200

    feat: implemented fs logic

--- sem_5_lab_1/main.py
+++ sem_5_lab_1/main.py
@@ -1,54 +1,110 @@
 from typing import List
 
 
-class Component:
-    def __init__(self, name: str):
-        self.__name__ = name
-        self.__max_size = -1
+class InvalidOperation(Exception):
+    pass
 
-    def is_composite(self) -> bool:
-        return False
+
+class Component:
+    def __init__(self, name: str, volume: int):
+        self.__name = name
+        self.__volume = volume  # component size
+        self.__content = []
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def max_size(self):
+        return self.__volume
 
     def push_record(self, record: str):
         raise NotImplementedError()
 
-    def read_file(self, name: str):
-        raise NotImplementedError()
-
     @property
-    def name(self):
-        return self.__name__
+    def content(self):
+        return self.__content
 
-    @property
-    def max_size(self):
-        return self.__max_size
+    @content.setter
+    def content(self, content):
+        self.__content = content
+
+    def display(self, ident: int):
+        print(' ' * ident, self.name, self.max_size)
+
+    def read_file(self):
+        print(self.name)
+        [print(x) for x in self.content]
 
 
 class BinaryFile(Component):
-    pass
+    def __init__(self, name: str, volume: int):
+        Component.__init__(self, name, volume)
+        self.content = ['fake content', 'you can look', "but you can't touch"]
+
+    def push_record(self, record: str):
+        raise Exception
 
 
 class LogTextFile(Component):
-    pass
+    def push_record(self, record: str):
+        self.content.append(record)
 
 
 class BufferFile(Component):
-    pass
+    def push_record(self, record: str):
+        self.content.append(record)
+
+    def read_file(self):
+        print(self.content.pop(0))
 
 
 class Composite(Component):
-    def __init__(self):
-        self.__components: List[Component] = []
-        self.__outputIdent = 3
+
+    def __init__(self, name: str, volume: int):
+        super().__init__(name, volume)
+        self.__components: [Component] = []
+
+    @property
+    def children(self):
+        return self.__components
+
+    def push_record(self, record: str):
+        raise Exception
 
     def create(self, component: Component):
-        pass
+        self.__components.append(component)
 
     def remove(self, component: Component):
-        pass
+        self.__components.remove(component)
 
-    def move(self, source: Component, destination: Component):
-        pass
+    @staticmethod
+    def move(source: Component, destination: Component):
+        destination = source
 
-    def get_children(self):
-        return self.__components
+    def read_file(self):
+        raise Exception
+
+    def display(self, ident: int):
+        print(f"{' ' * ident}{self.name}>")
+        ident += 3
+        [item.display(ident) for item in self.__components]
+
+
+defaultSize = 10
+
+tree = Composite("root", defaultSize)
+dir1 = Composite("dir1", defaultSize)
+dir2 = Composite("dir2", defaultSize)
+bin_file = BinaryFile("file1.bin", defaultSize)
+bin_file2 = BinaryFile("file11.bin", defaultSize)
+log_text_file = LogTextFile("file2.txt", defaultSize)
+buffer_file = BufferFile("file3.buff", defaultSize)
+
+tree.create(dir1)
+tree.create(dir2)
+dir1.create(bin_file)
+dir1.create(log_text_file)
+dir2.create(bin_file2)
+tree.create(buffer_file)
