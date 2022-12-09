6589a931c8fe7b59c4741dfee880fe73a74d8986
KhomenkoMac <maksim_khomenko@ukr.net>
2022-12-09 19:43:28 +0200

    fix:method signature coincidence

--- sem_5_lab_1/api.py
+++ sem_5_lab_1/api.py
@@ -7,28 +7,28 @@ root = Composite("root", 1000)
 
 # lists all inner objects
 @app.get('/directory')
-def get():
+def get_dir():
     pass
 
 
 # create directory
 # 204
 @app.post('/directory/<dir_name>/')
-def post(dir_name):
+def post_dir(dir_name):
     pass
 
 
 # move file or directory to subdirectory
 # 204 or 404
 @app.put('/directory/<from_dir_name>/<to_dir_name>')
-def put(from_dir_name, to_dir_name):
+def put_dir(from_dir_name, to_dir_name):
     pass
 
 
 # remove directory
 # 204 or 404
 @app.delete('/directory/<dir_name>')
-def delete(dir_name):
+def delete_dir(dir_name):
     pass
 
 
@@ -37,33 +37,33 @@ def delete(dir_name):
 
 # readfile
 @app.get('/file/<file_name>')
-def get():
+def get_file():
     pass
 
 
 # create file
 # body: volume
 @app.post('/file/<file_type>/<file_name>')
-def post(file_type, file_name):
+def post_file(file_type, file_name):
     pass
 
 
 # remove file
 @app.delete('/file/<file_name>')
-def delete(file_name):
+def delete_file(file_name):
     pass
 
 
 # move file
 @app.put('/file/<file_name>/<destination_dir>')
-def put(file_name, destination_dir):
+def put_file(file_name, destination_dir):
     pass
 
 
-# append a line
+# append a lineS
 # body: new_line_value
 @app.patch('/file/<file_name>')
-def patch():
+def patch_file():
     pass
 
 
