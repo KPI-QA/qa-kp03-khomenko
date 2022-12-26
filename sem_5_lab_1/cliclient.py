a685728a938d2ac9176369656dfd4cdd8283a1ee
vboxuser <vboxuser@uwu.myguest.virtualbox.org>
2022-12-26 15:51:06 +0200

    feat:cli client prototype

---
+++ sem_5_lab_1/cliclient.py
@@ -0,0 +1,20 @@
+API_DOMAIN = ""
+
+def read_component():
+    pass
+
+
+def create_directory():
+    pass
+
+
+def move_from_to():
+    pass
+
+
+def remove():
+    pass
+
+
+def writeto():
+    pass
